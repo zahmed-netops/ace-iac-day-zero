bab55827ca45fdebb33335a685078803b01d7f72
ahmed-netops <110470486+zahmed-netops@users.noreply.github.com>
2022-08-13 14:18:49 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "zahmed-netops"
+    organization = "zahmed-netops"
     workspaces {
       name = "ace-iac-day-zero"
     }
